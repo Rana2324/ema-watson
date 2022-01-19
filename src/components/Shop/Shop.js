561b45597f088814ddeae47c425c77b09684b65b
Rana2324 <shohelranatonmoy@gmail.com>
2022-01-20 03:30:35 +0900

    cratting a simple disign

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import Product from '../Product/Product';
+import './Shop.css';
 
 const Shop = () => {
-    
+    const first10 = fakeData.slice(0, 10);
+    const [products, setProducts] = useState(first10);
 
     return (
-        <div>
-            <h1>This is Shop</h1>
+        <div className='shop-container'>
+            <div className="product-container">
+                <ul>
+                    {
+                        products.map(pd => <Product product ={pd}></Product>)
+                    }
+                </ul>
+            </div>
+            <div className="cart-container">
+                <h3>This is cart</h3>
+            </div>
+
         </div>
     );
 };
